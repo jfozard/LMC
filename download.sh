e6f2f259d27ff5e307c173909e3da92ea24cd727
John Fozard <john.fozard@gmail.com>
2024-04-05 16:24:07 +0100

    Fix extract

--- download.sh
+++ download.sh
@@ -5,5 +5,5 @@ cd Orig_data/
 for s in {1..3} ; do
     
     wget "https://seafile.lirmm.fr/d/123f71e12bf24db59d84/files/?p=%2FStudy_${s}.tar.gz&dl=1" -O Study_${s}.tar.gz
-    tar xjf 
+    tar xjf Study_${s}.tar.gz
 done
