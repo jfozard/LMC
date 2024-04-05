2fc8d7735c22642705dff53484cb58b825ba522b
John Fozard <john.fozard@gmail.com>
2024-04-05 16:20:06 +0100

    Extract too

--- download.sh
+++ download.sh
@@ -1,4 +1,9 @@
 
-for s in {1..30} ; do
-echo wget https://seafile.lirmm.fr/d/123f71e12bf24db59d84/files/?p=%2FStudy_${s}.tar.gz&dl=1
+mkdir Orig_data/
+cd Orig_data/
+
+for s in {1..3} ; do
+    
+    wget "https://seafile.lirmm.fr/d/123f71e12bf24db59d84/files/?p=%2FStudy_${s}.tar.gz&dl=1" -O Study_${s}.tar.gz
+    tar xjf 
 done
