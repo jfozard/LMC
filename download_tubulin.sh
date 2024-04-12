142c51b8e30ef9caff90355d2620956d35f859c7
foz <foz>
2024-04-12 17:39:15 +0100

    Separate model for tubulin

---
+++ download_tubulin.sh
@@ -0,0 +1,8 @@
+
+mkdir Orig_data/
+cd Orig_data/
+
+for s in 4 7 17 20 21 24 29 30 ; do    
+    wget "https://seafile.lirmm.fr/d/123f71e12bf24db59d84/files/?p=%2FStudy_${s}.tar.gz&dl=1" -O Study_${s}.tar.gz
+    tar xjf Study_${s}.tar.gz
+done
