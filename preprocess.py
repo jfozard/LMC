49ddc00adc10b54acf0974f98281012ca2bbdb3b
foz <foz>
2024-04-16 00:14:18 +0100

    Stratified train-test splits

--- preprocess.py
+++ preprocess.py
@@ -32,7 +32,6 @@ for s in study_dirs:
     files = os.listdir(os.path.join(path, s))
     print(os.path.join(path, s), files)
     for f in files:
-        print(f)
         with tifffile.TiffFile(os.path.join(path, s, f)) as tif:
             metadata   = tif.ome_metadata # Get the existing metadata in a DICT
             metadata = xmltodict.parse(metadata)
@@ -57,18 +56,50 @@ for s in study_dirs:
 # Make test-train splits at this point, to ensure consistent between training steps
 
 images_by_output = { m:set() for m in ['Nucleus', 'Mitochondria', 'Actin', 'Tubulin'] }
+image_output_types = {}
 for i in images:
+    it = set()
     for p,m in images[i]['output_images']:
-	images_by_output[m].add(i)
+        images_by_output[m].add(i)
+        it.add(m)
+    image_output_types[i] = ''.join(sorted(it))
 
 print(images_by_output)
 
-# Make test-train split (non-stratified)
 
 
+# Make stratified test-train split
+
+from sklearn.model_selection import train_test_split
+
+TEST_SIZE = 0.1
+SEED = 42
+
+# generate indices: instead of the actual data we pass in integers instead
+train_indices, test_indices, _, _ = train_test_split(
+    list(images),
+    list(images),	
+    stratify=list(image_output_types.values()),
+    test_size=TEST_SIZE,
+    random_state=SEED
+)
+
+actin_train = [t for t in train_indices if t in images_by_output['Actin']]
+actin_test = [t for t in test_indices if t in images_by_output['Actin']]
+
+tubulin_train = [t for t in train_indices if t in images_by_output['Tubulin']]
+tubulin_test = [t for t in test_indices if t in images_by_output['Tubulin']]
 	
+splits = {
+    	'all':{'train':train_indices, 'test':test_indices},
+    	'Actin':{'train':actin_train, 'test':actin_test},
+    	'Tubulin':{'train':tubulin_train, 'test':tubulin_test}
+    	}
+    	
+with open(os.path.join(output_path, 'splits.pkl'), 'wb') as of:
+    pickle.dump(splits, of)
 
-
+print(splits)
 
 
 with open(os.path.join(output_path, 'images.pkl'), 'wb') as of:
@@ -76,3 +107,4 @@ with open(os.path.join(output_path, 'images.pkl'), 'wb') as of:
 
 with open(os.path.join(output_path, 'meta.pkl'), 'wb') as of:
     pickle.dump(meta, of)
+
